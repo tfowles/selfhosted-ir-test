136d1f28296eed781ae1d38492e08d7842ff582f
Thomas Fowles <thomas.fowles@gmail.com>
2020-12-15 22:46:19 -0700

    Update Install-Components.ps1

--- Install-Components.ps1
+++ Install-Components.ps1
@@ -5,7 +5,7 @@ Invoke-WebRequest -Uri "https://javadl.oracle.com/webapps/download/AutoDL?Bundle
 Invoke-WebRequest -Uri "https://download.microsoft.com/download/E/4/7/E4771905-1079-445B-8BF9-8A1A075D8A10/IntegrationRuntime_5.0.7632.4.msi" -OutFile "IntegrationRuntime.msi"
 
 # Install the JRE
-Start-Process -FilePath "$PSScriptRoot\JavaRuntimeEnvironment.exe" -ArgumentList INSTALLCFG="$PSScriptRoot\conf.ini"
+Start-Process -FilePath "$PSScriptRoot\JavaRuntimeEnvironment.exe" -ArgumentList INSTALLCFG="$PSScriptRoot\jre-conf.ini"
 
 # Install the Integration Runtime
 .\Setup-IntegrationRuntime.ps1 -path IntegrationRuntime.msi -authKey $IntegrationRuntimeAuthkey
